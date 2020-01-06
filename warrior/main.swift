07e2f4a2072b7aecde16be1f6892a7286bbba329
Saddam <saddam.satouyev@gmail.com>
2020-01-06 09:17:09 +0100

    Initial Commit

---
+++ warrior/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  warrior
+//
+//  Created by Saddam Satouyev on 06/01/2020.
+//  Copyright © 2020 Saddam Satouyev. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
